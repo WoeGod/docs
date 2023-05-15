f7ef778623d88d847bca1364a05c71bcccd7eae7
xuyinghua <923425525@qq.com>
2023-05-15 21:54:44 +0800

    feat: update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,42 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import Link from "next/link"
+// import Header from "./components/Header"
+import styled from "styled-components"
+import SearchBox from "./components/SearchBox";
+// import ProductList from "./components/ProductList";
+
 
 // const inter = Inter({ subsets: ['latin'] })
 
+const MainWrapper = styled.main`
+  background: #E5E5E5;
+  padding: 72px 46px;
+`
+
+const Title = styled.h1`
+    font-size: 44px;
+    line-height: 64px;
+    margin: 0;
+    margin-bottom: 40px;
+`
+
+const Description = styled.div`
+  margin-bottom: 30px;
+`
+
 export default function Home() {
   return (
-    <main>
-      <Link href={`/docs`}>doucment</Link>
-    </main>
+    <>
+      {/* <Header /> */}
+      <MainWrapper>
+        <Title>Technical documentation</Title>
+        <Description>Search for in-depth articles on Qixin developer tools and technologies.</Description>
+        <SearchBox />
+      </MainWrapper>
+      {/* <ProductList /> */}
+    </>
+
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
     //     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
