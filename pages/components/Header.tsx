c2cb099e1667a822ba4eb3fbe600c8ad41b104d0
xuyinghua <923425525@qq.com>
2023-05-16 22:07:52 +0800

    feat: update Header style

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -12,18 +12,20 @@ const HeaderWrapper = styled.div`
 const LeftWrapper = styled.div`
   display: flex;
 `
+
 const TopTitle = styled.h3`
-  color: rgb(0,0,0);
   font-size: 16px;
   margin: 0;
   padding: 0 20px;
 `
-const LogoTitle = styled(TopTitle)`
+const LogoTitle = styled(Link)`
+  font-weight: bold;
+  font-size: 16px;
+  margin: 0;
+  padding: 0 20px;
   border-right: 2px solid #000000;
 `
 const StyledLink = styled(Link)`
-  color: #000000;
-  text-decoration: unset;
   &: hover {
     text-decoration: underline;
   }
@@ -34,7 +36,7 @@ const Logo = () => {
   return (
     <div className="flex align-center">
       <Image width={30} src={require('../../public/logo.png')} alt="logo" />
-      <LogoTitle><StyledLink href={'/'}>QiXin</StyledLink></LogoTitle>
+      <LogoTitle href="/">QiXin</LogoTitle>
     </div>
   );
 };
